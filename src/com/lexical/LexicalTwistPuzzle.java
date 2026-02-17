a36d51004d6ad6d9748d84dd09f66025bac11aef
gargsparsh07 <sparshgarg.pks@gmail.com>
2026-02-17 21:49:12 +0530

    [Sparsh]: UC2: Accept Word Inputs

--- src/com/lexical/LexicalTwistPuzzle.java
+++ src/com/lexical/LexicalTwistPuzzle.java
@@ -1,8 +1,19 @@
 package com.lexical;
 
+import java.util.Scanner;
+
 public class LexicalTwistPuzzle {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Lexical Twist Puzzle");
+
+        System.out.print("Enter first word: ");
+        String firstWord = scanner.nextLine();
+
+        System.out.print("Enter second word: ");
+        String secondWord = scanner.nextLine();
     }
 }
